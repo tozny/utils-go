380d0efbc30ac815a80d91af7c1782adad03debd
Levi Sky Schoen <levi@tozny.com>
2019-04-11 15:38:22 -0700

    Extract common functionality for service and health check into generic methods.

--- http.go
+++ http.go
@@ -153,3 +153,12 @@ func CORSHandler(h http.Handler, corsHeaders []http.Header) http.Handler {
 		h.ServeHTTP(w, r)
 	})
 }
+
+// HealthCheckHandler handles health check requests to the specified service
+// returning 200 if the service is up, otherwise nothing
+func HealthCheckHandler(serviceName string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("%s service is up.\n", serviceName)))
+	})
+}
