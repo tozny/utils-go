c9a50a2940c67cefb71d609c1774d81f0ce48198
michael <leemiwill@gmail.com>
2019-04-12 15:16:45 -0700

    turning off logging levels

--- logger.go
+++ logger.go
@@ -6,28 +6,49 @@ import (
 	"os"
 )
 
+const (
+	OFF      = iota // 0
+	SERVICE  = iota
+	CRITICAL = iota
+	ERROR    = iota
+	INFO     = iota
+	DEBUG    = iota // 5
+)
+
 // ServiceLogger represents a logger with logging level prefixes for a specific service.
 type ServiceLogger struct {
 	ServiceName string
-	Debug       *log.Logger
-	Info        *log.Logger
-	Fatal       *log.Logger
-	Error       *log.Logger
 	*log.Logger
+	Critical *log.Logger
+	Error    *log.Logger
+	Info     *log.Logger
+	Debug    *log.Logger
 }
 
 // NewServiceLogger returns a logger with designated logging levels for a particular service.
-func NewServiceLogger(serviceName string, debug bool) ServiceLogger {
+func NewServiceLogger(serviceName string, level int) ServiceLogger {
 	logger := ServiceLogger{
 		serviceName,
-		log.New(os.Stdout, "DEBUG: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
-		log.New(os.Stdout, "INFO: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
-		log.New(os.Stdout, "FATAL: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
-		log.New(os.Stdout, "ERROR: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
 		log.New(os.Stdout, serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
+		log.New(os.Stdout, "CRITICAL: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
+		log.New(os.Stdout, "ERROR: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
+		log.New(os.Stdout, "INFO: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
+		log.New(os.Stdout, "DEBUG: "+serviceName+": ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds),
 	}
-	if debug == false {
+	if level < DEBUG {
 		logger.Debug.SetOutput(ioutil.Discard)
+	}
+	if level < INFO {
+		logger.Info.SetOutput(ioutil.Discard)
+	}
+	if level < ERROR {
+		logger.Error.SetOutput(ioutil.Discard)
+	}
+	if level < CRITICAL {
+		logger.Critical.SetOutput(ioutil.Discard)
+	}
+	if level < SERVICE {
+		logger.SetOutput(ioutil.Discard)
 	}
 	return logger
 }
