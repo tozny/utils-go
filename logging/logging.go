97d919d9f71c68b268a913a0647e3d4f74970a25
Luke Woodward <luke@tozny.com>
2019-05-02 06:35:30 -0700

    channel is a go keyword

--- logging/logging.go
+++ logging/logging.go
@@ -6,16 +6,16 @@ import (
 	"os"
 )
 
-// LogWriter maps string values to io.Writer channels intended for logging output.
+// LogWriter maps string values to io.Writer interfaces intended for logging output.
 //
 // This function is intended to provide a standard way of mapping environment-based
-// configuration with various logging channels. An empty string will default to
+// configuration with various logging output writers. An empty string will default to
 // standard out. stdout, stderr will send output to standard out and standard error
 // respectively. /dev/null will discard the output. Any other string will provide
 // a writer to a file at that location.
 //
 // When calling this function, it is a good idea to type assert for an io.Closer
-// and if assertion is successful properly close the log file on shutdown.
+// or similar and if assertion is successful properly close the log file on shutdown.
 func LogWriter(writerString string) (io.Writer, error) {
 	switch writerString {
 	case "stdout", "":
