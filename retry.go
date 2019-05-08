e3ceecf55bfdc41a4af4aed1992a3f3fc0505b19
Eli Fabens <efabens@tozny.com>
2019-05-08 16:06:54 -0700

    Updates retry logic to be more verbally correct
    - Max tries now reflects the maximum number of tries done. (I would also be open to renaming the variable maxRetries and leaving that logic the same)
    - On the last try doesn't sit around and escapes early

--- retry.go
+++ retry.go
@@ -12,13 +12,17 @@ type Ready func() bool
 // Await waits until the ready function is ready
 // or errors, returning success and error (if any).
 // To stop waiting, send on the stop channel.z
-func Await(ready Ready, maxTries int) bool {
-	for tries := 0; tries <= maxTries; tries++ {
+// It checks if the function is ready once and then retries
+// the specified number of times with an exponential backoff between each attempt
+func Await(ready Ready, maxRetries int) bool {
+	for tries := 0; tries <= maxRetries; tries++ {
 		success := ready()
 		if !success {
-			// exponentially back off before the next attempt
-			// https://github.com/adonovan/gopl.io/blob/77e9f810f3c2502e9c641b97e09f9721424090f5/ch5/wait/wait.go#L30
-			time.Sleep((1 * time.Second) << uint(tries))
+			if tries != maxRetries {
+				// exponentially back off before the next attempt
+				// https://github.com/adonovan/gopl.io/blob/77e9f810f3c2502e9c641b97e09f9721424090f5/ch5/wait/wait.go#L30
+				time.Sleep((1 * time.Second) << uint(tries))
+			}
 			continue
 		}
 		return true
