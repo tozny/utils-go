50b51362b23a9ecfdc26fa6b79ab317a0bcab623
Levi Sky Schoen <levi@tozny.com>
2019-07-02 14:57:33 -0700

    Add redis cache client that abstracts construction of clustered/unclustered client.

---
+++ cache/redis.go
@@ -0,0 +1,41 @@
+// Package cache contains concrete implementations of clients for interacting with cache datastores.
+package cache
+
+import (
+	"crypto/tls"
+
+	"github.com/go-redis/redis"
+)
+
+// Config wraps configuration for a redis client.
+type Config struct {
+	Address            string
+	Password           string
+	ClusterModeEnabled bool
+	TLSEnabled         bool
+}
+
+// NewClient returns a new redis client configured with the provided config.
+func NewClient(config Config) redis.Cmdable {
+	var client redis.Cmdable
+	if config.ClusterModeEnabled {
+		redisOpts := &redis.ClusterOptions{
+			Addrs:    []string{config.Address},
+			Password: config.Password,
+		}
+		if config.TLSEnabled {
+			redisOpts.TLSConfig = &tls.Config{}
+		}
+		client = redis.NewClusterClient(redisOpts)
+	} else {
+		redisOpts := &redis.Options{
+			Addr:     config.Address,
+			Password: config.Password,
+		}
+		if config.TLSEnabled {
+			redisOpts.TLSConfig = &tls.Config{}
+		}
+		client = redis.NewClient(redisOpts)
+	}
+	return client
+}
