e0e43ac35c2c465efc43f2aa6c17044fac30b662
Luke Woodward <luke@tozny.com>
2020-12-10 15:35:37 -0800

    Update error message -- props @willmichael

--- database/db.go
+++ database/db.go
@@ -69,7 +69,7 @@ func (d dbLogger) BeforeQuery(q *pg.QueryEvent) {
 func (d dbLogger) AfterQuery(q *pg.QueryEvent) {
 	query, err := q.FormattedQuery()
 	if err != nil {
-		d.logger.Errorf("error %q executing query:\n%+v ", err, query)
+		d.logger.Errorf("error %q formatting query:\n%+v ", err, query)
 		return
 	}
 	start, ok := q.Ctx.Value(dlTimingKey).(time.Time)
